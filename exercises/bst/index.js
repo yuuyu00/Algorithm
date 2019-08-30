dddef5330a025f260cedea8d1c489c1e5b171e9e
yu <my219813@gmail.com>
2019-08-30 17:58:52 +0900

    anagrams, fizzbuzz, bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,41 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = {};
+    this.right = {};
+  }
+  insert(data, node = this) {
+    if (!node || !node.data) {
+      node.data = data;
+      return node;
+    }
+
+    if (data < node.data) {
+      node.left = this.insert(data, "left" in node ? node.left : new Node());
+    } else {
+      node.right = this.insert(data, "right" in node ? node.right : new Node());
+    }
+
+    return node;
+  }
+
+  contains(data, node = this) {
+    if (node.data === data) return node;
+    if (!node.data) return null;
+
+    if (data < node.data) {
+      return !("left" in node) || !node.left.data
+        ? null
+        : this.contains(data, node.left);
+    } else {
+      return !("right" in node) || !node.right.data
+        ? null
+        : this.contains(data, node.right);
+    }
+  }
+}
 
 module.exports = Node;
